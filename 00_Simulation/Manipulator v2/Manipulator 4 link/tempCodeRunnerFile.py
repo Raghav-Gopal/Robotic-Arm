1a7599fe34ef2e377809b7d03ad729efc5b79ea3
Raghav-Gopal <mraghavgopal@gmail.com>
2025-06-22 14:09:05 +0530

    Added v3 files

--- 00_Simulation/Manipulator v2/Manipulator 4 link/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-os(((l1**2)-(l2**2)+((Intermediate_set[0])**2)+((Intermediate_set[1])**2)+((Intermediate_set[2])**2))/(2*l1*(math.sqrt((((Intermediate_set[0])**2)+((Intermediate_set[1])**2)+((Intermediate_set[2])**2)))))) + math.atan(Intermediate_set[2]/math.sqrt((Intermediate_set[0]**2)+(Intermediate_set[1]**2)))
-# theta_2 = math.acos(((l1**2)-(l2**2)+((Intermediate_set[0])**2)+((Intermediate_set[1])**2)+((Intermediate_set[2])**2))/(2*l1*(math.sqrt((((Intermediate_set[0])**2)+((Intermediate_set[1])**2)+((Intermediate_set[2])**2)))))) + math.acos(-1*(
-#     ((l1*2)-(l2**2)-(((Intermediate_set[0])**2)+((Intermediate_set[1])**2)+((Intermediate_set[2])**2)))
-#     /
-#     (2*l2*(math.sqrt(((Intermediate_set[0])**2)+((Intermediate_set[1])**2)+((Intermediate_set[2])**2))))))
-# theta_3 = math.at
